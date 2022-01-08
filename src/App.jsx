f8dba90cfe8781d00f44450ec74e6e687da9064e
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-01-08 14:33:40 +0200

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
-import Test from './components/Test/Test';
+import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
+import TestPage from "./pages/TestPage/TestPage";
 
 class App extends Component {
-  render() { 
-    return <Test/>;
+  render() {
+    return (
+      <Routes>
+        <Route path="/test" element={<TestPage />} />
+      </Routes>
+    );
   }
 }
- 
+
 export default App;
